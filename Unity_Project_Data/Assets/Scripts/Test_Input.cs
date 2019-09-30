db7750c9e5cca5cca921f1dbd9074f63327365a0
741PC-37\student <cu.common@gmail.com>
2019-09-30 16:03:56 +0900

    ２Pコントローラー対応

--- Unity_Project_Data/Assets/Scripts/Test_Input.cs
+++ Unity_Project_Data/Assets/Scripts/Test_Input.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Test_Input : MonoBehaviour
 {
-	private string[] Input_Name = new string[12]
+	private string[] Bottom_Name = new string[24]
 	{
+		"GamePad_1_0",
 		"GamePad_1_1",
 		"GamePad_1_2",
 		"GamePad_1_3",
@@ -17,7 +18,34 @@ public class Test_Input : MonoBehaviour
 		"GamePad_1_9",
 		"GamePad_1_10",
 		"GamePad_1_11",
-		"GamePad_1_12",
+		"GamePad_2_0",
+		"GamePad_2_1",
+		"GamePad_2_2",
+		"GamePad_2_3",
+		"GamePad_2_4",
+		"GamePad_2_5",
+		"GamePad_2_6",
+		"GamePad_2_7",
+		"GamePad_2_8",
+		"GamePad_2_9",
+		"GamePad_2_10",
+		"GamePad_2_11",
+	};
+
+	private string[] Axis_Name = new string[12]
+	{
+		"GamePad_1_AxisL_X",
+		"GamePad_1_AxisL_Y",
+		"GamePad_1_AxisR_X",
+		"GamePad_1_AxisR_Y",
+		"GamePad_1_POV_X",
+		"GamePad_1_POV_Y",
+		"GamePad_2_AxisL_X",
+		"GamePad_2_AxisL_Y",
+		"GamePad_2_AxisR_X",
+		"GamePad_2_AxisR_Y",
+		"GamePad_2_POV_X",
+		"GamePad_2_POV_Y",
 	};
 
     void Start()
@@ -28,11 +56,23 @@ public class Test_Input : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        foreach(var name in Input_Name)
+        foreach(var name in Bottom_Name)
 		{
 			if(Input.GetButtonDown(name))
 			{
 				Debug.Log(name);
+			}
+		}
+        foreach(var name in Axis_Name)
+		{
+			var num = Input.GetAxis(name);
+			if (num > 0.0f)
+			{
+				Debug.Log(name + ": +");
+			}
+			else if (num < 0.0f)
+			{
+				Debug.Log(name + ": -");
 			}
 		}
     }
