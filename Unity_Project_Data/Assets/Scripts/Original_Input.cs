32512b7bea0aa38c31b34e3dc6fd790fbd918153
wanarisu <wanarisu@gmail.com>
2019-09-30 20:44:08 +0900

    ごめんなさい、外部からインプット触れないの修正

--- Unity_Project_Data/Assets/Scripts/Original_Input.cs
+++ Unity_Project_Data/Assets/Scripts/Original_Input.cs
@@ -6,44 +6,41 @@
  *　2019/09/30：ドッキングゲームのオリジナル入力名で設定できるプロパティ群
  *　2019/09/30：XInput適応
  */
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // ドッキングゲームオリジナル入力返し
 namespace DockingGame_Input
 {
-	public class Original_Input : MonoBehaviour
+	public class Original_Input
 	{
-
 		// GamePad1の入力群----------------------------------------------
 		#region ABXY
-		public bool ButtomA { get { return Input.GetButton("GamePad_1_0"); } }
-		public bool ButtomB { get { return Input.GetButton("GamePad_1_1"); } }
-		public bool ButtomX { get { return Input.GetButton("GamePad_1_2"); } }
-		public bool ButtomY { get { return Input.GetButton("GamePad_1_3"); } }
+		static public bool ButtomA	{ get { return Input.GetButton("GamePad_1_0"); } }
+		static public bool ButtomB	{ get { return Input.GetButton("GamePad_1_1"); } }
+		static public bool ButtomX	{ get { return Input.GetButton("GamePad_1_2"); } }
+		static public bool ButtomY	{ get { return Input.GetButton("GamePad_1_3"); } }
 		#endregion
 		#region トリガー
-		public bool ButtomLeft1		{ get { return Input.GetButton("GamePad_1_4"); } }
-		public bool ButtomRight1	{ get { return Input.GetButton("GamePad_1_5"); } }
-		public bool ButtomLeft2		{ get { return Input.GetAxis("GamePad_1_Axis_3") < 0.0f; } }
-		public bool ButtomRight2	{ get { return Input.GetAxis("GamePad_1_Axis_3") > 0.0f; } }
+		static public bool ButtomLeft1	{ get { return Input.GetButton("GamePad_1_4"); } }
+		static public bool ButtomRight1	{ get { return Input.GetButton("GamePad_1_5"); } }
+		static public bool ButtomLeft2	{ get { return Input.GetAxis("GamePad_1_Axis_3") < 0.0f; } }
+		static public bool ButtomRight2	{ get { return Input.GetAxis("GamePad_1_Axis_3") > 0.0f; } }
 		#endregion
 		#region LRボタン
-		public bool ButtomL3 { get { return Input.GetButton("GamePad_1_8"); } }
-		public bool ButtomR3 { get { return Input.GetButton("GamePad_1_9"); } }
+		static public bool ButtomL3	{ get { return Input.GetButton("GamePad_1_8"); } }
+		static public bool ButtomR3	{ get { return Input.GetButton("GamePad_1_9"); } }
 		#endregion
 		#region その他
-		public bool Buttom6	{ get { return Input.GetButton("GamePad_1_6"); } }
-		public bool Buttom7	{ get { return Input.GetButton("GamePad_1_7"); } }
+		static public bool Buttom6	{ get { return Input.GetButton("GamePad_1_6"); } }
+		static public bool Buttom7	{ get { return Input.GetButton("GamePad_1_7"); } }
 		#endregion
 		#region 軸系
-		public float StickLeft_X	{ get { return Input.GetAxis("GamePad_1_Axis_1"); } }
-		public float StickLeft_Y	{ get { return Input.GetAxis("GamePad_1_Axis_2"); } }
-		public float StickRight_X	{ get { return Input.GetAxis("GamePad_1_Axis_4"); } }
-		public float StickRight_Y	{ get { return Input.GetAxis("GamePad_1_Axis_5"); } }
-		public float Cross_X		{ get { return Input.GetAxis("GamePad_1_Axis_6"); } }
-		public float Cross_Y		{ get { return Input.GetAxis("GamePad_1_Axis_7"); } }
+		static public float StickLeft_X		{ get { return Input.GetAxis("GamePad_1_Axis_1"); } }
+		static public float StickLeft_Y		{ get { return Input.GetAxis("GamePad_1_Axis_2"); } }
+		static public float StickRight_X	{ get { return Input.GetAxis("GamePad_1_Axis_4"); } }
+		static public float StickRight_Y	{ get { return Input.GetAxis("GamePad_1_Axis_5"); } }
+		static public float Cross_X			{ get { return Input.GetAxis("GamePad_1_Axis_6"); } }
+		static public float Cross_Y			{ get { return Input.GetAxis("GamePad_1_Axis_7"); } }
 		#endregion
 		// GamePad1の入力群----------------------------------------------
 	}
--- Unity_Project_Data/Assets/Scripts/Test_Input.cs
+++ Unity_Project_Data/Assets/Scripts/Test_Input.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DockingGame_Input;
 
 public class Test_Input : MonoBehaviour
 {
