37fc7566b4eee29ff5c62f0477e0ea01176480ce
741PC-15\student <cu.common@gmail.com>
2019-10-01 11:58:47 +0900

    コード挙動つくったぞ、LineRendererとかParticleとかうまく使えないのが痛いなって

--- Unity_Project_Data/Assets/Scripts/Yamazaki/Test_Input_2.cs
+++ Unity_Project_Data/Assets/Scripts/Yamazaki/Test_Input_2.cs
@@ -15,14 +15,56 @@ public class Test_Input_2 : MonoBehaviour
 	public float maxAddBoostPower;
 	public float addOil;
 
+	// 前情報がわかるためのPrefab
+	public GameObject prevPosPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+	// 前の情報
+	public struct PrevInfo
+	{
+		// 前の位置
+		public Vector3 prevPos;
+		// 前の角度
+		public Quaternion prevRot;
+		// 前の位置オブジェクト
+		public GameObject prevPosObj;
+	}
+
+	public PrevInfo[] prevInfos = new PrevInfo[500];
+
+	// 距離間隔
+	public float distanceInterval;
+	
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		for (int i = 0; i < prevInfos.Length; i++)
+		{
+			prevInfos[i].prevPosObj = Instantiate(prevPosPrefab);
+			prevInfos[i].prevPosObj.transform.position = prevInfos[i].prevPos = transform.position;
+			prevInfos[i].prevPosObj.transform.rotation = prevInfos[i].prevRot = transform.rotation;
+		}
+	}
 
 	// Update is called once per frame
 	void Update()
+	{
+		AngleUpdate();
+		BoostUpdate();
+
+		if(Vector2.Distance(transform.position, prevInfos[0].prevPos) >= distanceInterval)
+		{
+			for (int i = prevInfos.Length - 1; i > 0; i--)
+			{
+				prevInfos[i].prevPosObj.transform.position = prevInfos[i].prevPos = prevInfos[i - 1].prevPos;
+				prevInfos[i].prevPosObj.transform.rotation = prevInfos[i].prevRot = prevInfos[i - 1].prevRot;
+			}
+			prevInfos[0].prevPosObj.transform.position = prevInfos[0].prevPos = transform.position;
+			prevInfos[0].prevPosObj.transform.rotation = prevInfos[0].prevRot = transform.rotation;
+		}
+    }
+
+	private void AngleUpdate()
 	{
 		if (Input.GetKey(KeyCode.A))
 		{
@@ -34,13 +76,6 @@ public class Test_Input_2 : MonoBehaviour
 		{
 			Debug.Log("うえ");
 			addZAngle += addNum;
-
-		}
-
-		if (Input.GetKey(KeyCode.Space))
-		{
-			Debug.Log("すすむ");
-			addBoostPower += addOil;
 		}
 
 		if (addZAngle > maxAddZAngle)
@@ -52,6 +87,18 @@ public class Test_Input_2 : MonoBehaviour
 			addZAngle = -maxAddZAngle;
 		}
 
+		transform.Rotate(0.0f, 0.0f, addZAngle);
+		addZAngle *= 1.0f - decreaseRate;
+	}
+
+	private void BoostUpdate()
+	{
+		if(Input.GetKey(KeyCode.Space))
+		{
+			Debug.Log("すすむ");
+			addBoostPower += addOil;
+		}
+
 		if (addBoostPower > maxAddBoostPower)
 		{
 			addBoostPower = maxAddBoostPower;
@@ -61,10 +108,7 @@ public class Test_Input_2 : MonoBehaviour
 			addBoostPower = -maxAddBoostPower;
 		}
 
-		transform.Rotate(0.0f, 0.0f, addZAngle);
 		transform.Translate(new Vector3(1.0f, 0.0f, 0.0f) * addBoostPower);
-
-		addZAngle *= 1.0f - decreaseRate;
 		addBoostPower *= 1.0f - decreaseRate;
-    }
+	}
 }
