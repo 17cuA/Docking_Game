6720cd6e845588301fb4c206c94ea3906115c78a
741PC-17\student <uroncha.15.3@gmail.com>
2019-10-03 13:14:58 +0900

    カメラが追う

--- Unity_Project_Data/Assets/Scripts/kawamura/CameraWork.cs
+++ Unity_Project_Data/Assets/Scripts/kawamura/CameraWork.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class CameraWork : MonoBehaviour
 {
 	public GameObject chargerObj;
+	public float defPosZ_Value;
 	public float posZ;
+
+	public float rotaX_Max;
+	public float rotaX_Min;
+	public float rotaY_Max;
+	public float rotaY_Min;
 
 	void Start()
 	{
@@ -14,7 +20,7 @@ public class CameraWork : MonoBehaviour
 
 	void Update()
 	{
-		posZ = chargerObj.transform.position.z - 2.2f;
+		posZ = chargerObj.transform.position.z - defPosZ_Value;
 		transform.position = new Vector3(transform.position.x, transform.position.y, posZ);
 	}
 }
