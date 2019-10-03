b897e4b467dbbd3c8973ccbd5cb891799681a026
741PC-17\student <uroncha.15.3@gmail.com>
2019-10-03 12:46:30 +0900

    カメラの追従（回転はまだ）

---
+++ Unity_Project_Data/Assets/Scripts/kawamura/CameraWork.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour
+{
+	public GameObject chargerObj;
+	public float posZ;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		posZ = chargerObj.transform.position.z - 2.2f;
+		transform.position = new Vector3(transform.position.x, transform.position.y, posZ);
+	}
+}
