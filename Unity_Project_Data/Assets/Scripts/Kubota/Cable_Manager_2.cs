919f3bb9a9776718d2df4a622b6e4373e0fa1a7e
741PC-12\student <cu.common@gmail.com>
2019-10-03 13:11:19 +0900

    久保田がやりました

--- Unity_Project_Data/Assets/Scripts/Kubota/Cable_Manager_2.cs
+++ Unity_Project_Data/Assets/Scripts/Kubota/Cable_Manager_2.cs
@@ -6,10 +6,16 @@ public class Cable_Manager_2 : MonoBehaviour
 {
 	private float speed;
 	public float Speed_Max;
+	public float decreaseRate;
 
-	//public float 
-    // Start is called before the first frame update
-    void Start()
+	private float addXNum;
+	private float addYNum;
+	private float addZNum;
+
+	public float addNum;
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
@@ -17,6 +23,57 @@ public class Cable_Manager_2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		Forward_Move();
+		Movement();
+		Charger_Move();
+
+	}
+
+	void Movement()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			addYNum += addNum;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			Debug.Log("左");
+			addXNum += -addNum;
+
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			Debug.Log("下");
+			addYNum += -addNum;
+
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			Debug.Log("右");
+			addXNum += addNum;
+
+		}
+	}
+
+	void Forward_Move()
+	{
+		if(Input.GetKey(KeyCode.X))
+		{
+			addZNum += addNum;
+		}
+		if(Input.GetKey(KeyCode.Z))
+		{
+			addZNum += -addNum;
+		}
+
+	}
+
+	void Charger_Move()
+	{
+		transform.Translate(new Vector3(addXNum, addYNum, addZNum) * Speed_Max);
+		//addZNum *= 1.0f - decreaseRate;
+		//addXNum *= 1.0f - decreaseRate;
+		//addYNum *= 1.0f - decreaseRate;
+
+	}
 }
