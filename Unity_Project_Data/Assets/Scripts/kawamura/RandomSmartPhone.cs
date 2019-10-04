6667fc696ca1b88b2acdda2b1ce468a7ff118c61
741PC-17\student <uroncha.15.3@gmail.com>
2019-10-04 18:13:07 +0900

    らんだむ

---
+++ Unity_Project_Data/Assets/Scripts/kawamura/RandomSmartPhone.cs
@@ -0,0 +1,34 @@
+﻿//作成者：川村良太
+//作成日：2019/10/04
+//スマホをランダムな位置にする処理
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSmartPhone : MonoBehaviour
+{
+	[Header("入力用　Xのランダム出現範囲の最大値")]
+	public float randomX_ValueMax;
+	[Header("入力用　Xのランダム出現範囲の最小値")]
+	public float randomX_ValueMin;
+	[Header("入力用　Yのランダム出現範囲の最大値")]
+	public float randomY_ValueMax;
+	[Header("入力用　Yのランダム出現範囲の最小値")]
+	public float randomY_ValueMin;
+
+	float posX;
+	float posY;
+
+    void Start()
+    {
+		posX = Random.Range(randomX_ValueMin, randomX_ValueMax);
+		posY = Random.Range(randomY_ValueMin, randomY_ValueMax);
+
+		transform.position = new Vector3(posX, posY, 0);
+    }
+
+    void Update()
+    {
+        
+    }
+}
