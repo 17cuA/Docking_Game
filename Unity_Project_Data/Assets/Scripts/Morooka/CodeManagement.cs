5c5754fbd51f69a746d54f3ae2e6ed7120ee41c1
wanarisu <wanarisu@gmail.com>
2019-10-05 19:48:12 +0900

    コードの追従2

--- Unity_Project_Data/Assets/Scripts/Morooka/CodeManagement.cs
+++ Unity_Project_Data/Assets/Scripts/Morooka/CodeManagement.cs
@@ -8,10 +8,9 @@ public class CodeManagement : MonoBehaviour
 	[SerializeField, Tooltip("コードのターゲット")] private GameObject codeTarget;
 	[SerializeField, Tooltip("充電器本体のスクリプト")] private Charger_Manager ChargerScript;
 
-	private List<Transform> CodesTransforms { get; set; }
-	private List<bool> IsACertainDistance { get; set; }
-
-	private float CodesLocalPositionInterval = -0.046f;
+	private List<Transform> CodesTransforms { get; set; }		// コードと先端のTransformのリスト
+	private List<bool> IsACertainDistance { get; set; }			// コードが自分の前のコードと一定離れているかどうか
+	private float FollowUpSpeed { get; set; }					// 追従速度
 
 	void Start()
     {
@@ -28,6 +27,8 @@ public class CodeManagement : MonoBehaviour
 		
 		// 子供のPositionなどの設定
 		float zPos = 0.0f;
+		float CodesLocalPositionInterval = -0.046f;
+
 		for (int i = 0; i < tempTransform.Count; i++)
 		{
 			// ポジション設定
@@ -45,8 +46,11 @@ public class CodeManagement : MonoBehaviour
 
     void Update()
     {
+		// 追従速度は先端の速度に比例
+		FollowUpSpeed = ChargerScript.NowSpeed / 50.0f;
+
 		// 移動するのか確認
-        for(int i = 1; i < CodesTransforms.Count;i++)
+		for (int i = 1; i < CodesTransforms.Count;i++)
 		{
 			// 一定の位置にいる判定のとき
 			if(!IsACertainDistance[i-1])
@@ -67,8 +71,8 @@ public class CodeManagement : MonoBehaviour
 				float temp = CodesTransforms[i + 1].transform.position.z;
 
 				CodesTransforms[i + 1].transform.position 
-					= new Vector3( Mathf.MoveTowards( CodesTransforms[i + 1].transform.position.x, CodesTransforms[i].transform.position.x, ChargerScript.NowSpeed / 50.0f),
-					Mathf.MoveTowards(CodesTransforms[i + 1].transform.position.y, CodesTransforms[i].transform.position.y, ChargerScript.NowSpeed / 50.0f),
+					= new Vector3( Mathf.MoveTowards( CodesTransforms[i + 1].transform.position.x, CodesTransforms[i].transform.position.x, FollowUpSpeed),
+					Mathf.MoveTowards(CodesTransforms[i + 1].transform.position.y, CodesTransforms[i].transform.position.y, FollowUpSpeed),
 					CodesTransforms[i + 1].transform.position.z);
 
 				if((Vector2)CodesTransforms[i + 1].transform.position == (Vector2)CodesTransforms[i].transform.position)
@@ -83,5 +87,12 @@ public class CodeManagement : MonoBehaviour
 		Vector3 temp = transform.position;
 		temp.z = codeTarget.transform.position.z;
 		transform.position = temp;
+
+		// 各コードの向き指定
+		for(int i = 1; i < CodesTransforms.Count; i++)
+		{
+			Quaternion targetRotation = Quaternion.LookRotation(CodesTransforms[i-1].position - CodesTransforms[i].position);
+			CodesTransforms[i].rotation = Quaternion.Slerp(CodesTransforms[i].rotation, targetRotation, FollowUpSpeed);
+		}
 	}
 }
