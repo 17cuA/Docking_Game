b770a4ee31a6be8ab574129e007cecb1b5c8fb96
tatsuotsu <tatsuyij@gmail.com>
2019-10-05 17:47:01 +0900

    道筋途中

---
+++ Unity_Project_Data/Assets/Scripts/Kubota/Root_Light_Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Root_Light_Manager : MonoBehaviour
+{
+	[SerializeField] GameObject Hal9000;
+	[SerializeField] GameObject Chager;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void calc_distance()
+	{
+		Vector3 a = Hal9000.transform.position;
+		Vector3 b = Hal9000.transform.position;
+
+		float distance = Mathf.Sqrt(Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2) + Mathf.Pow(a.z - b.z, 2));
+	}
+}
