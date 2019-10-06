17d6c3cdddf2499f870d54afbf2db7a2860cf22d
tatsuotsu <tatsuyij@gmail.com>
2019-10-06 16:50:00 +0900

    ゲームが止まるバグ

--- Unity_Project_Data/Assets/Scripts/Kubota/Root_Light_Manager.cs
+++ Unity_Project_Data/Assets/Scripts/Kubota/Root_Light_Manager.cs
@@ -6,16 +6,21 @@ public class Root_Light_Manager : MonoBehaviour
 {
 	[SerializeField] GameObject Hal9000;
 	[SerializeField] GameObject Chager;
+
+	public GameObject Root_Light;
     void Start()
     {
 		Debug.Log( calc_distance());
-    }
+		calc_distance();
+		make_distance();
 
-    // Update is called once per frame
-    void Update()
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-        
-    }
+
+	}
 
 	//距離の計算
 	float calc_distance()
@@ -23,7 +28,15 @@ public class Root_Light_Manager : MonoBehaviour
 		Vector3 a = Hal9000.transform.position;
 		Vector3 b = Chager.transform.position;
 
-
 		return Mathf.Sqrt(Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2) + Mathf.Pow(a.z - b.z, 2));
+	}
+	void make_distance()
+	{
+		int num = Mathf.FloorToInt(calc_distance());
+
+		for (int i = num; num > 0; i--)
+		{
+			Instantiate(Root_Light, new Vector3(0, Hal9000.transform.position.y, Hal9000.transform.position.z - num), Quaternion.identity);
+		}
 	}
 }
