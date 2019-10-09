f3a297aca473d4ab59af5b85eed0b79550939b9a
741PC-41\student <cu.common@gmail.com>
2019-10-09 18:49:06 +0900

    新しいモデルと誘導灯の配置

--- Unity_Project_Data/Assets/Scripts/Kubota/Target_Manager.cs
+++ Unity_Project_Data/Assets/Scripts/Kubota/Target_Manager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Target_Manager : MonoBehaviour
 {
+
+	Vector3[] Target_Pos;
+	[SerializeField] GameObject[] Target;
+
+	[SerializeField] GameObject Charger;            //充電器（unity側にて設定）
+	float distance;
+	int Target_cnt;
     // Start is called before the first frame update
     void Start()
     {
-        
+		for(int i = 0; i < Target.Length; i++)
+		{
+			Target_Pos[i] = Target[i].transform.position;
+		}
+		Target_cnt = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		if (Vector3.Distance(Target_Pos[Target_cnt], Charger.transform.position) > distance)
+		{
+
+			Next_Target();
+		}
+	}
+
+	private void Next_Target()
+	{
+		Target[Target_cnt].SetActive(false);
+		Target_cnt += 1;
+		if(Target_cnt < Target.Length)
+		{
+			Target[Target_cnt].SetActive(true);
+		}
+	}
+	public int Get_InRadius()
+	{
+		return Target_cnt;
+	}
 }
