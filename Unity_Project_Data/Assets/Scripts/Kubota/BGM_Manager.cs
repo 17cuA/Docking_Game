b6ac7dd3f2aff74e942d4aaa3cd5ef74d5220415
tatsuotsu <tatsuyij@gmail.com>
2019-10-10 23:38:08 +0900

    いったん保存

--- Unity_Project_Data/Assets/Scripts/Kubota/BGM_Manager.cs
+++ Unity_Project_Data/Assets/Scripts/Kubota/BGM_Manager.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class BGM_Manager : MonoBehaviour
 {
+	public static BGM_Manager BGM_obj;
+
 	[Header("呼吸音を鳴らしているもの")]
 	public AudioSource Breathing;
 	[Header("無線を鳴らすもの")]
 	public AudioSource Wireless;
 	public AudioClip Wireless_AudioClip;
-    // Start is called before the first frame update
-    void Start()
+	[Header("ドッキングに使うもの")]
+	public AudioSource Docking;
+	public AudioClip Docking_Audioclip;
+
+	void Awake()
+	{
+		BGM_obj = GetComponent<BGM_Manager>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
 		Breathing.Play();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	/// <summary>
+	/// 無線を鳴らす際に使用する
+	/// </summary>
 	void Active_Wireless()
 	{
 		Wireless.PlayOneShot(Wireless_AudioClip);
+	}
+	/// <summary>
+	/// ドッキング時に鳴らすもの
+	/// </summary>
+	void Sound_Docking()
+	{
+		Docking.PlayOneShot(Docking_Audioclip);
 	}
 }
