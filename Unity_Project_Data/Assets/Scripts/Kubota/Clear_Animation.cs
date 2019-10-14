c54cff5e31196dd6806fc26274816e56113e07fc
tatsuotsu <tatsuyij@gmail.com>
2019-10-14 17:06:34 +0900

    作業途中

---
+++ Unity_Project_Data/Assets/Scripts/Kubota/Clear_Animation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clear_Animation : MonoBehaviour
+{
+	public RectTransform Maskpos;
+
+	public float speed;
+	public float speed_Max;
+	public float add_speed;
+
+	//色
+	public bool nowColorA;
+	public Color colorA;
+	public Color colorB;
+	public float elapsedTime;         //経過時間
+	public float cycleTime;         //周期時間
+	public SpriteRenderer spriteRenderer;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		nowColorA = true;
+		elapsedTime = 0.0f;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		move_obj();
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime >= cycleTime)
+		{
+			if (nowColorA) spriteRenderer.color = colorA;
+			else spriteRenderer.color = colorB;
+			elapsedTime = 0.0f;
+			nowColorA = !nowColorA;
+		}
+
+		Add_Speed();
+	}
+
+	void move_obj()
+	{
+		Maskpos.position = new Vector3(Maskpos.position.x, Maskpos.position.y, Maskpos.position.z - speed);
+	}
+	void Add_Speed()
+	{
+		if(speed < speed_Max)
+		{
+			speed += add_speed;
+		}
+	}
+}
